ad750a59a060856c60f978a1059c7c3af33e4dad
jnicolasdz <jndiazs@udistrital.edu.co>
2026-05-16 20:36:03 -0500

    fix(/Model/Algorithms/MaximumFlow/): adding args: residual graphs and sink to implemented EdmondsKarp Algorithm

--- GraphRepresentation/Graphs/src/main/java/com/udistrital/Model/Algorithms/MaximumFlow/BFS.java
+++ GraphRepresentation/Graphs/src/main/java/com/udistrital/Model/Algorithms/MaximumFlow/BFS.java
@@ -1,48 +1,47 @@
 package com.udistrital.Model.Algorithms.MaximumFlow;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
 import com.udistrital.Model.Graph.Graph;
 import com.udistrital.Model.Vertex.IVertex;
 
-public class BFS<T,U> {
+public class BFS<T, U extends Number & Comparable<U>> {
 
     private Graph<T,U> graph;
     public BFS(Graph<T,U> graph) {
         this.graph = graph;
     }
 
-    public List<IVertex<T,U>> bfs(T originValue) {
+    public boolean bfs(IVertex<T, U> source, IVertex<T, U> sink, Map<IVertex<T, U>, Map<IVertex<T, U>, Double>> residual, Map<IVertex<T, U>, IVertex<T, U>> parent) {
 
-        IVertex<T,U> origin = this.graph.getVertex(originValue);
-        Map<IVertex<T,U>,Boolean> visitedArray = new HashMap<>();
-
-        for (IVertex<T,U> vertex : graph.getVertexs()) {
-            visitedArray.put(vertex, false);
+        Map<IVertex<T, U>, Boolean> visited = new HashMap<>();
+        for (IVertex<T, U> v : graph.getVertexs()) {
+            visited.put(v, false);
         }
-
-        List<IVertex<T,U>> order = new ArrayList<>();
-        Queue<IVertex<T,U>> queue = new LinkedList<>();
-
-        visitedArray.put(origin, true);
-        queue.offer(origin);
+        Queue<IVertex<T, U>> queue = new LinkedList<>();
+        visited.put(source, true);
+        parent.put(source, null);
+        queue.offer(source);
 
         while (!queue.isEmpty()) {
-            IVertex<T,U> vertex = queue.poll();
-            order.add(vertex);
-            for (IVertex<T,U> neighbor : this.graph.matrixAdya().get(vertex)) {
-                    if (!visitedArray.get(neighbor)) {
-                        visitedArray.put(neighbor, true);
-                        queue.offer(neighbor);
+            IVertex<T, U> u = queue.poll();
+
+            for (IVertex<T, U> v : graph.getVertexs()) {
+                if (!visited.get(v)) {
+                    double cap = residual.get(u).getOrDefault(v, 0.0);
+                    if (cap > 0) {
+                        visited.put(v, true);
+                        parent.put(v, u);
+                        if (v.equals(sink)) return true;
+                        queue.offer(v);
                     }
                 }
             }
+        }
 
-            return order;
-    } 
+        return false;
+    }
 }
